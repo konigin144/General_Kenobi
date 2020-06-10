7dec5015637c812d4e7eadb8129fc9898dbe8978
konigin144 <kingask777@wp.pl>
2020-06-10 20:37:51 +0200

    disconnects after hello there

--- kenobi.py
+++ kenobi.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord.voice_client import VoiceClient
 from discord import FFmpegPCMAudio
 from discord.utils import get
+from asyncio import sleep
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,26 +14,12 @@ TC_TOKEN = os.getenv('TC_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
 
+async def af(vc):
+    await vc.disconnect(force=True)
+
 @bot.command()
 async def test(ctx):
     print('xd')
-
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user.name}')
-
-@bot.event
-async def on_voice_state_update(member, before, after):
-    if before.channel is None and after.channel is not None:
-        if after.channel.id == VC_TOKEN:
-            print(member)
-            channel = bot.get_channel(TC_TOKEN)
-            await channel.send('hello')
-            #channel = member.message.author.voice.channel
-            channelv = bot.get_channel(162681501203300353)
-            vc = await channelv.connect()
-            vc.play(discord.FFmpegPCMAudio(source="file.mp3"))
-            #TODO: Disconnect
 
 @bot.command(pass_context=True)
 async def join(ctx):
@@ -50,7 +37,28 @@ async def play(ctx):
     channel = ctx.message.author.voice.channel
     vc = await channel.connect()
     vc.play(discord.FFmpegPCMAudio(source="file.mp3"))
+    while vc.is_playing():
+        await sleep(1)
+    await vc.disconnect()
 
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user.name}')
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if before.channel is None and after.channel is not None:
+        if after.channel.id == int(VC_TOKEN):
+            #channel = bot.get_channel(720005745369677906)
+            #await channel.send('hello')
+            #channel = member.message.author.voice.channel
+            channelv = bot.get_channel(int(VC_TOKEN))
+            vc = await channelv.connect()
+            vc.play(discord.FFmpegPCMAudio(source="file.mp3"))
+            while vc.is_playing():
+                await sleep(1)
+            await vc.disconnect()
+        
 bot.run(TOKEN)
 
 #TODO: Error handling
